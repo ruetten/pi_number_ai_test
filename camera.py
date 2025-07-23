aab11a9dd007eb18d70cb05b758cfbfae2d95bb3
250006359 <laik.ruetten@gehealthcare.com>
2025-07-22 19:39:09 -0500

    open cv version

--- camera.py
+++ camera.py
@@ -1,9 +1,13 @@
 from picamera2 import Picamera2
+import cv2
 
 picam2 = Picamera2()
-picam2.start_preview()
 picam2.start()
 
-input("Press Enter to stop...")
+while True:
+    cv2.imshow("Camera", picam2.capture_array())
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 picam2.stop()
+cv2.destroyAllWindows()
